ba562a6ca202f9348a8ca3db9ca489f2edc36849
ankiano <ankiano@users.noreply.github.com>
2020-02-12 10:04:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3.6
 # coding=utf-8
 
-from pip._internal.req import parse_requirements
-from pip._internal.download import PipSession
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.download import PipSession
+except ImportError: # for pip <= 9.0.3
+    from pip.download import PipSession
 
 from setuptools import setup, find_packages
 
@@ -15,7 +21,7 @@ requirements = [str(ir.req) for ir in parsed_requirements]
 
 setup(
     name='etl',
-    version='0.0.15',
+    version='0.0.16',
     description='ETL CLI tool',
     long_description='Extract transform load CLI tool for extracting small '
                      'and middle data volume from sources (databases, '
