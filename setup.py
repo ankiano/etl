7f47254d89f8776185767428de4c4f5cee1bf6dc
ankiano <a.akinshyn@gmail.com>
2020-10-07 10:33:49 +0300

    added the ability to inject parameters into query templates

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [str(ir.req) for ir in parsed_requirements]
 
 setup(
     name='etl',
-    version='0.0.21',
+    version='0.0.22',
     description='ETL CLI tool',
     long_description='Extract transform load CLI tool for extracting small '
                      'and middle data volume from sources (databases, '
