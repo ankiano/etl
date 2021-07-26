7d58302a0ec2f47a15303138940151aed254e9a7
ankiano <ankiano@users.noreply.github.com>
2021-07-26 13:39:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with Path('requirements.txt').open() as requirements_txt:
 
 setup(
     name='etl',
-    version='0.0.32',
+    version='0.0.33',
     description='ETL CLI tool',
     long_description='Extract transform load CLI tool for extracting small '
                      'and middle data volume from sources (databases, '
