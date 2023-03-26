da2d76fdcb53e02d8d0dd63cb1e2bbb0b31cf479
ankiano <a.akinshyn@gmail.com>
2023-03-26 10:51:31 +0300

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,9 @@ release = '1.0.5'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +26,5 @@ language = 'python'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
