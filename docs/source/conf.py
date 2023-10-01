28ace20ebbaa7030aabc04a7209d7347c1ed847e
ankiano <a.akinshyn@gmail.com>
2023-10-01 20:49:15 +0300

    added documentation for sharepoint excel usage

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'etl-tool'
 copyright = '2023, ankiano'
 author = 'ankiano'
-release = '1.0.5'
+release = '1.0.6'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
